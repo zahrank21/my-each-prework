c51b8c56445f822d897c99d79fdcfe4d00453670
zahrank21 <zahrank@gmail.com>
2017-11-20 14:47:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+arr = [1,2,3,4]
+
+def my_each(arr)
+  counter = 0
+  result = []
+  while counter < arr.length
+    result << arr[counter] * 2
+    yield(arr[counter])
+    counter += 1
+  end
+  arr
 end
+
+my_each(arr) {|el| el}
